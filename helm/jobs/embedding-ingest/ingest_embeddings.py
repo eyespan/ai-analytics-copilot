32919ee98620c184d6e014e58fdfe8e7e905071f
yohannesmeasho <yohannes.measho@1nce.com>
2026-07-26 15:12:33 +0100

    fix clickhouse host access issue from embedding

--- helm/jobs/embedding-ingest/ingest_embeddings.py
+++ helm/jobs/embedding-ingest/ingest_embeddings.py
@@ -11,7 +11,8 @@ from opensearchpy.helpers import bulk
 
 #CLICKHOUSE_HOST = "clickhouse"
 CLICKHOUSE_HOST = (
-    "clickhouse.data.svc.cluster.local"
+    "clickhouse-shard1-0."
+    "clickhouse-headless.data.svc.cluster.local"
 )
 CLICKHOUSE_PORT = 9000
 CLICKHOUSE_DATABASE = "github"
